94f180da795751158a61bf19efaa3bff34f6dc4a
jeanps22 <jean.philip22@hotmail.com>
2025-01-21 15:16:22 -0300

    ++ Teste CI

--- car-wash-api/WeatherForecast.cs
+++ car-wash-api/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace car_wash_api
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.55565);
 
         public string? Summary { get; set; }
     }
